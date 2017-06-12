d68d8cc3238d4bf4cd80a809e506b394daf07b0f
NguyenThanhTuan1234 <tuankunbeo@gmail.com>
2017-06-12 09:47:04 +0700

    Sua file Learn_java.java

--- Bank.java
+++ Bank.java
@@ -30,6 +30,6 @@ AGR i=new AGR();
 CTG a=new CTG();  
 System.out.println("VCB Rate of Interest: "+s.getRateOfInterest());  
 System.out.println("AGR Rate of Interest: "+i.getRateOfInterest());  
-System.out.println("CTG Rate of Interest: "+a.getRateOfInterest());  
+System.out.println("CTG Rate of Interest: "+a.getRateOfInterest()); 
 }  
 }  
